b71ac0543fa60ae62a4493f7a721a8e53eee184a
Manuel Schwarz <dermanuelschwarz@gmail.com>
2024-05-16 17:20:42 +0200

    Added Calculator and Main class. Calculator does exactly what you would think he does, in a very simple manner. Also added the adapted pom.xml file

--- src/main/java/at/campus02/bsd/Calculator.java
+++ src/main/java/at/campus02/bsd/Calculator.java
@@ -1,4 +1,27 @@
+/*
+Author: Manuel Schwarz
+Usage: Calculator
+Last Change: 16.05.24
+ */
 package at.campus02.bsd;
 
 public class Calculator {
+
+    public static double add(double a, double b) {
+        return a + b;
+    }
+
+    public static double minus(double a, double b) {
+        return a - b;
+    }
+
+    public static double divide(double a, double b) {
+        return a / b;
+    }
+
+    public static double multiply(double a, double b) {
+        return a * b;
+    }
+
+
 }
---
+++ src/main/java/at/campus02/bsd/Main.java
@@ -0,0 +1,12 @@
+package at.campus02.bsd;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println(Calculator.add(1,1));
+        System.out.println(Calculator.minus(1,1));
+        System.out.println(Calculator.divide(4,2));
+        System.out.println(Calculator.multiply(4,2));
+    }
+}
