427df625c61c1d8a6d22536779069e3cef0ac829
Manuel Schwarz <52301600@edu.campus02.at>
2024-05-27 14:00:43 +0200

    Added logging dependencies in pom.xml; new log4j2.xml file for console output of logfiles; screenshot of first logging output; createt Logger Object in Main.java

--- src/main/java/at/campus02/bsd/Main.java
+++ src/main/java/at/campus02/bsd/Main.java
@@ -1,6 +1,11 @@
 package at.campus02.bsd;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 public class Main {
+
+    private static Logger logger = LogManager.getLogger();
 
     public static void main(String[] args) {
 
@@ -8,6 +13,9 @@ public class Main {
         double resultMinus = Calculator.minus(1,1);
         double resultDivide = Calculator.divide(4,2);
         double resultMultiply = Calculator.multiply(4,2);
+
+        logger.info("info");
+        logger.error("error");
 
         System.out.println("Manuel Schwarz");
     }
